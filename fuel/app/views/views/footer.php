ce6306a6cd25a53802ac3b11dc6e0bd3f1d43bfb
meganedogYuu <tkra.yuuki@gmail.com>
2016-06-14 16:54:42 +0900

    add - headerとfooterをテンプレートから読み込めるようにした

---
+++ fuel/app/views/views/footer.php
@@ -0,0 +1,3 @@
+<div>
+	<?php echo "ここはfooterの領域です"; ?>
+</div>
---
+++ fuel/app/views/views/header.php
@@ -0,0 +1,20 @@
+<div>
+	<div class="navi">
+
+
+		<ul class="navi_list">
+			<li class="navi_item">
+				<a href="analysis">
+					<img
+						src="http://www.google.co.jp/logos/doodles/2016/karl-landsteiners-148th-birthday-5693101206142976-res.png"
+						class="navi_logo">
+				</a>
+			</li>
+			<li class="navi_item"><a href="analysis">分析</a></li>
+			<li class="navi_item"><a href="#">イベント</a></li>
+			<li class="navi_item"><a href="user/alluser">ユーザ設定</a></li>
+			<li class="navi_item"><a href="readFile">csv読み込み</a></li>
+		</ul>
+
+	</div>
+</div>
