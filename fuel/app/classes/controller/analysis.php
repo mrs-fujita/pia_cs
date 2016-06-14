5ef009e817feb403b39fbde03b031a450191cf98
meganedogYuu <tkra.yuuki@gmail.com>
2016-06-14 16:55:28 +0900

    change - analysis.phpをテンプレートを使用するように変更

--- fuel/app/classes/controller/analysis.php
+++ fuel/app/classes/controller/analysis.php
@@ -1,16 +1,22 @@
 <?php
 
-class Controller_Analysis extends Controller
+class Controller_Analysis extends Controller_Template
 {
 	public function action_index()
 	{
+		$this->template = View::forge('template');
+
+
 		// ログインをしているかなどの処理
 
 		// 選択されたチームを判定する処理
 
 		// 必要なデータを取得
 		$data["test"] = "test";
-		return Response::forge(View::forge('analysis/index',$data));
+		$this->template->title = "test";
+		$this->template->content = View::forge('analysis/index', $data);
+
+		//return Response::forge(View::forge('analysis/index',$data));
 	}
 }
 ?>
--- fuel/app/views/analysis/index.php
+++ fuel/app/views/analysis/index.php
@@ -1,15 +1,1 @@
-<!doctype html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<title>Document</title>
-</head>
-<body>
 <p>analysis/index</p>
-
-<a href="user/alluser">ユーザ一覧</a>
-
-<a href="readFile">csv読み込み</a>
-
-</body>
-</html>
