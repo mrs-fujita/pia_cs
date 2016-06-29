13c90f3917153453efbc6dfe1989617473b861e4
meganedogYuu <tkra.yuuki@gmail.com>
2016-06-29 23:53:42 +0900

    add - サイドバーの大きなカテゴリを選択した時の処理を記述

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,22 @@
-
-
+/**
+ * 分析画面でサイドバーのメニューを選択した時の処理
+ */
 $(function() {
 
-  var $child = $(".js-selectBoxChild");
+  var $parents = $(".js-selectBoxParent");
+  var $children = $(".js-selectBoxChildren");
 
-  $child.hide();
+  var $parentLink = $(".js-selectBoxParent > a");
+
+  // サイドバーのメニューの大きなカテゴリをクリックされた時の処理
+  $parentLink.click(function(e) {
+    // クリックされたサイドバーの要素の番号
+    var num = $parentLink.index(this);
+
+    // 子要素を表示する
+    $children.eq(num).toggle();
+
+    // リンク先へ飛ぶのを無効化
+    e.preventDefault();
+  });
 });
