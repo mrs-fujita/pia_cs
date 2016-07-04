ed97e8114b93314fdf7f23a5b8157a2c69eaa2b9
funimu <hpmp4949@gmail.com>
2016-07-04 17:57:40 +0900

    add category controller and model and views

---
+++ fuel/app/classes/controller/category.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.8
+ * @author     Mrs_fujita
+ * @license    MIT License
+ * @copyright  2010 - 2016 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+/**
+ * The category Controller.
+ *
+ * @package  app
+ * @extends  Controller
+ */
+class Controller_Event extends Controller
+{
+	/**
+	 * The index page controller
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_index()
+	{
+		$data["category"] = Model_Event::find_all();
+		return Response::forge(View::forge('category/index',$data));
+	}
+	/**
+   *
+   *The detail page controller
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	// public function action_detail()
+	// {
+  //   $get = Input::get();
+	// 	$id = $get["id"];
+	// 	$data["details"] = Model_Event::find_by('id',$id);
+	// 	return Response::forge(View::forge('event/detail',$data));
+	// }
+
+	/**
+	 * The category add page controller
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_add()
+	{
+		return Response::forge(View::forge('category/add'));
+	}
+
+	/**
+	 * The category add page controller
+	 *
+	 * @access  public
+	 * @return  Response
+	 */
+	public function action_adddo()
+	{
+    $post = Input::post();
+		$add = Model_Event::post_add($post);
+		$data["msg"] = "失敗しました。";
+		if($update){
+			$data["msg"] = "成功しました。";
+		}
+		$data["category"] = Model_Event::find_all();
+		return Response::forge(View::forge('category/index',$data));
+	}
+
+  /**
+   * The update page controller
+   *
+   * @access  public
+   * @return  Response $data
+   */
+  public function action_update()
+  {
+		$get = Input::get();
+		$id = $get["id"];
+		$data["category"] = Model_Event::find_by('id',$id);
+    return Response::forge(View::forge('category/update',$data));
+  }
+
+	/**
+   * The update page controller
+   *
+   * @access  public
+   * @return  Response $data
+   */
+  public function action_updatedo()
+  {
+    $post = Input::post();
+		$id = $post["id"];
+    $entry = Model_Event::find_by_pk($post["id"]);
+    $data["msg"] = "更新に失敗しました。";
+    if($entry){
+			$entry["name"] = $post["name"];
+			$entry["color"] = $post["color"];
+			$entry->save();
+    }
+		//var_dump($entry);
+		$data["category"] = Model_Event::find_by('id',$id);
+    return Response::forge(View::forge('category/index',$data));
+	}
+
+  /**
+   * The delete method controller
+   *
+   * @access  public
+   * @return  Response $data
+   */
+  public function action_delete()
+  {
+    $post = Input::post();
+    $entry = Model_Event::find_by_pk($post["id"]);
+		if ($entry){
+		    $entry->delete();
+		}
+		$data["category"] = Model_Event::find_all();
+		return Response::forge(View::forge('category/index',$data));
+  }
+
+}
