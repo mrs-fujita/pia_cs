dfb92b59a616a9b9bce7f5702aa8cd8e34d771c4
pojimako <66527mako@gmail.com>
2016-07-06 18:03:15 +0900

    追加時にパスワードの整合性を確認

--- fuel/app/classes/controller/admin.php
+++ fuel/app/classes/controller/admin.php
@@ -68,27 +68,37 @@ class Controller_Admin extends Controller_App
 
 			その辺はこのコントローラーの他のアクションや、その他のコントローラー参照でやって
 			*/
+			//パスワードの整合性を確認
+			if($post["password"] == $post["password_confirm"]){
+				//パスワードが一致した場合
+				// 新規adminを作成
+				$admin = Model_Admin::forge()->set(array(
+					"authority_id" => 1,
+					"name" => $post["name"],
+					"password" => $post["password"],
+					"regist_day" => date('Y-m-d'),
+					"change_day" => date('Y-m-d'),
+					"available_startday" => $post["start_time"],
+					"available_endday" => $post["end_time"],
+				));
 
-			// 新規adminを作成
-			$admin = Model_Admin::forge()->set(array(
-				"authority_id" => 1,
-				"name" => $post["name"],
-				"password" => $post["password"],
-				"regist_day" => date('Y-m-d'),
-				"change_day" => date('Y-m-d'),
-				"available_startday" => $post["start_time"],
-				"available_endday" => $post["end_time"],
-			));
-
-			if($admin->save())
-			{
-				//登録できた時の処理
-				Response::redirect('admin/index');
-			}else
-			{
-				//登録失敗した時の処理
-				Response::redirect('admin/index');
+				if($admin->save())
+				{
+					//登録できた時の処理
+					Response::redirect('admin/index');
+				}
+				else
+				{
+					//登録失敗した時の処理
+					Response::redirect('admin/index');
+				}
 			}
+
+			else {
+				//パスワードが不一致の場合
+				Response::redirect('admin/create');
+			}
+
 
 		}
 		else
